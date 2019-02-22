b61bc09592bd08489e5603ff986427269f9a2e49
CodyM4V3R1CK <bobkomatej1@gmail.com>
2019-02-22 20:57:11 +0100

    add / calculator - switch version

---
+++ JavaScript/LEARNING SHIT/calculator - switch version.js
@@ -0,0 +1,16 @@
+let calculator=function(a,b,c){
+    switch (b){
+        case "+" :
+            return a+c;
+        case "-" :
+            return a-c;
+        case "*" :
+            return a*c;
+        case "/" :
+            if (c==0){
+                console.log("Error, can't divide by zero")
+            }
+            return (a/c)
+    }
+}
+console.log(calculator(12,"/",0))
