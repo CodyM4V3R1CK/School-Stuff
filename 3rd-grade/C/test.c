dabda7c7e412a48b4c1f26fb080b3327123b5adf
CodyM4V3R1CK <bobkomatej1@gmail.com>
2020-09-21 11:30:44 +0200

    smol scan&print

--- 3rd-grade/C/test.c
+++ 3rd-grade/C/test.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main(void){
+    int a;
 
     printf("Hello World \n");
+    printf("zadaj cislo: ");
+    scanf("%d", &a);
+    printf("tvoje cislo je: %d\n", a);
     return 0;
 }
