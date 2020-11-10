eafd9da68b39ac5ab24101f75b6186ebd9824795
CodyM4V3R1CK <bobkomatej1@gmail.com>
2020-11-10 14:45:52 +0100

    no idea

--- 3rd-grade/Java/firstJavaFX/src/sample/Main.java
+++ 3rd-grade/Java/firstJavaFX/src/sample/Main.java
@@ -1,28 +1,38 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    /*
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception {
+        // TODO Auto-generated method stub
+        Button btn1=new Button("Say, Hello World");
+        btn1.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent arg0) {
+                // TODO Auto-generated method stub
+                System.out.println("hello world");
+            }
+        });
+        StackPane root=new StackPane();
+        root.getChildren().add(btn1);
+        Scene scene=new Scene(root,600,400);
+        primaryStage.setTitle("First JavaFX Application");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
-    */
-
-    Button btn1 = new Button("Say, Hello World");
-
-
-    public static void main(String[] args) {
+    public static void main (String[] args)
+    {
         launch(args);
     }
 }
