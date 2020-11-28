09ee69bf0e6d90df4ae7a81a0b1c9b1cd622a4bd
CodyM4V3R1CK <bobkomatej1@gmail.com>
2020-11-28 20:18:02 +0100

    nejakym zazrakom bere f1 az f4, ale este nie jak by mal

--- 3rd-grade/C/game1/main.c
+++ 3rd-grade/C/game1/main.c
@@ -2,7 +2,15 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <time.h>
+#include <termio.h>
+#include <unistd.h>
+#include <fcntl.h>
 //#include <conio.h>
+
+int getch(void);
+int kbhit(void);
+
+
 
 int main() {
     int a = 0, b = 0, hadanyZnak = 0;
@@ -10,29 +18,38 @@ int main() {
 
     srand(time(NULL));
 
-    while(a != 48){
-        printf("F1 - Napoveda \t F2 - Start/Restart \t F10/ESC - Koniec\n"); //zatial 1 2 a 0
-        scanf("%s", &a);
+    while(getch()!= 83){
+        printf("F1 - Napoveda \t F2 - Start/Restart \t F4/ESC - Koniec\n"); //zatial 1 2 a 0
+        a = getch();
+//        printf("%d", a);
+        if(a == 27){
+            a = getch();
+//            printf("%d", a);
+            if(a == 79){
+                a = getch();
+//                printf("%d", a);
+            }
+        }
 //        printf("%d\n", a);
 
 // prve menu
 
-        if (a == 49){
+        if (a == 80){ //49
             printf("Meranie bude spocivat v tom, ze program vygeneruje nahodny znak, zobrazi ho a spusti");
             printf("meranie casu. \nMeranie sa zastavi v okamihu, ked uzivatel stlaci rovnaku klavesu. \n");
             printf("k uzivatel stlaci nieco ine, program to ma ignorovat. \nNapokon program vypise zmerany cas,");
             printf("ktory uplynie od vypisania po stlacenie klavesy.\n");
-        }else if(a == 48){
+        }else if(a == 83){ //48
             return 0;
-        }else if(a == 50){
+        }else if(a == 81){ //50
             goto game;
         }
 
 //menu po 1
 
-        scanf("%s", &b);
+        b = getch();
         game:
-        if(b == 50){
+        if(b == 81){
             system("clear"); //clear terminal
             char randomLetter = (rand()%(122 - 97 + 1)) + 97; //ascii kod pismena
             char hadaneZnaky[30];
@@ -42,8 +59,8 @@ int main() {
 //  hadame znak a zistujeme cas
             gettimeofday(&start, NULL);
             do {
-                scanf("%s", &hadanyZnak);
-                if(hadanyZnak == 48){
+                hadanyZnak = getch();
+                if(hadanyZnak == 83){
                     return 0;
                 }
                 hadaneZnaky[pocetPokusov] = hadanyZnak;
@@ -105,105 +122,46 @@ int main() {
                     }
                 }
             }
-
-        }else if(b == 48){
+        }else if(b == 83){
             return 0;
         }
         printf("\n");
     }
 }
 
-//    printf("F1 - Napoveda \t F2 - Start/Restart \t F10/ESC - Koniec\n"); //zatial 1 2 a 0
-//    scanf("%s", &a);
-//    printf("%d\n", a);
-//
-//// prve menu
-//
-//    if (a == 49){
-//        printf("Meranie bude spocivat v tom, ze program vygeneruje nahodny znak, zobrazi ho a spusti meranie casu. Meranie sa zastavi v okamihu,"
-//               "ked uzivatel stlaci rovnaku klavesu. Ak uzivatel stlaci nieco ine, program to ma ignorovat. Napokon program vypise zmerany cas, "
-//               "ktory uplynie od vypisania po stlacenie klavesy.\n");
-//    }else if(a == 48){
-//        return 0;
-//    }
-//
-////menu po 1
-//
-//    scanf("%s", &b);
-//    if(b == 50){
-////        system("clear"); //clear terminal
-//        char randomLetter = (rand()%(122 - 97 + 1)) + 97; //ascii kod pismena
-//        char hadaneZnaky[30];
-//        int pocetPokusov = 0;
-//        printf("%c\n", randomLetter);
-//
-////  hadame znak a zistujeme cas
-//        t = clock();
-//        do {
-//            scanf("%s", &hadanyZnak);
-//            if(hadanyZnak == 48){
-//                return 0;
-//            }
-//            hadaneZnaky[pocetPokusov] = hadanyZnak;
-//            pocetPokusov++;
-//        }while (hadanyZnak != randomLetter);
-//        t = clock() - t;
-//        double time = ((double)t)/CLOCKS_PER_SEC; //sekundy
-////  vyherne hlasky
-//        if(pocetPokusov == 1){
-//            if(time <= 1){
-//                printf("\33[0;32m"); //green
-//                printf("cas bol: %f\n", time);
-//                printf("\33[0m");
-//                printf("stlacili ste: %c", hadanyZnak);
-//            }else if(time > 1 && time <= 1.5){
-//                printf("\33[0;36m"); //cyan
-//                printf("cas bol: %f\n", time);
-//                printf("\33[0m");
-//                printf("stlacili ste: %c", hadanyZnak);
-//            }else if(time > 1.5){
-//                printf("\33[0;31m"); //red
-//                printf("cas bol: %f\n", time);
-//                printf("\33[0m");
-//                printf("stlacili ste: %c", hadanyZnak);
-//            }
-//        }else if(pocetPokusov >= 1){
-//            if(time <= 1){
-//                printf("\33[0;32m");
-//                printf("cas bol: %f\n", time);
-//                printf("\33[0m");
-//                printf("Postupne ste stlacali: ");
-//                int i = 0;
-//                while(i <= pocetPokusov){
-//                    printf("%c ", hadaneZnaky[i]);
-//                    i++;
-//                }
-//            }else if(time > 1 && time <= 1.5){
-//                printf("\33[0;36m");
-//                printf("cas bol: %f\n", time);
-//                printf("\33[0m");
-//                printf("Postupne ste stlacali: ");
-//                int i = 0;
-//                while(i <= pocetPokusov){
-//                    printf("%c ", hadaneZnaky[i]);
-//                    i++;
-//                }
-//            }else if(time > 1.5){
-//                printf("\33[0;31m");
-//                printf("cas bol: %f\n", time);
-//                printf("\33[0m");
-//                printf("Postupne ste stlacali: ");
-//                int i = 0;
-//                while(i <= pocetPokusov){
-//                    printf("%c ", hadaneZnaky[i]);
-//                    i++;
-//                }
-//            }
-//        }
-//
-//    }else if(b == 48){
-//        return 0;
-//    }
-//
-//    return 0;
-//}
+int kbhit(void){
+    struct termios oldt, newt;
+    int ch;
+    int oldf;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+    if(ch != EOF){
+        ungetc(ch, stdin);
+        return 1;
+    }
+
+    return 0;
+}
+
+int getch(void){
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr( STDIN_FILENO, &oldattr );
+    newattr = oldattr;
+    newattr.c_lflag &= ~( ICANON | ECHO );
+    tcsetattr( STDIN_FILENO, TCSANOW, &newattr );
+    ch = getchar();
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldattr );
+    return ch;
+}
