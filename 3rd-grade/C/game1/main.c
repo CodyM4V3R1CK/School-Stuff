96e7017fe165d849020cfbc281747d2889f90894
CodyM4V3R1CK <bobkomatej1@gmail.com>
2020-11-28 20:34:01 +0100

    IT WORKS

--- 3rd-grade/C/game1/main.c
+++ 3rd-grade/C/game1/main.c
@@ -17,115 +17,126 @@ int main() {
     struct timeval start, stop;
 
     srand(time(NULL));
+    printf("F1 - Napoveda \t F2 - Start/Restart \t F4/ESC - Koniec\n");
 
-    while(getch()!= 83){
-        printf("F1 - Napoveda \t F2 - Start/Restart \t F4/ESC - Koniec\n"); //zatial 1 2 a 0
-        a = getch();
-//        printf("%d", a);
-        if(a == 27){
+    if(!kbhit()){
+        while(getch()!= 83){
+            printf("F1 - Napoveda \t F2 - Start/Restart \t F4 - Koniec\n");
             a = getch();
-//            printf("%d", a);
-            if(a == 79){
+//            printf("%d\n", a);
+            if(a == 27){
                 a = getch();
-//                printf("%d", a);
+//                printf("%d\n", a);
+                if(a == 79){
+                    a = getch();
+//                    printf("%d\n", a);
+                }
             }
-        }
 //        printf("%d\n", a);
 
 // prve menu
 
-        if (a == 80){ //49
-            printf("Meranie bude spocivat v tom, ze program vygeneruje nahodny znak, zobrazi ho a spusti");
-            printf("meranie casu. \nMeranie sa zastavi v okamihu, ked uzivatel stlaci rovnaku klavesu. \n");
-            printf("k uzivatel stlaci nieco ine, program to ma ignorovat. \nNapokon program vypise zmerany cas,");
-            printf("ktory uplynie od vypisania po stlacenie klavesy.\n");
-        }else if(a == 83){ //48
-            return 0;
-        }else if(a == 81){ //50
-            goto game;
-        }
+            if (a = 79 || a == 80){ //49
+                system("clear");
+                printf("Meranie bude spocivat v tom, ze program vygeneruje nahodny znak, zobrazi ho a spusti");
+                printf("meranie casu. \nMeranie sa zastavi v okamihu, ked uzivatel stlaci rovnaku klavesu. \n");
+                printf("k uzivatel stlaci nieco ine, program to ma ignorovat. \nNapokon program vypise zmerany cas,");
+                printf("ktory uplynie od vypisania po stlacenie klavesy.\n");
+                printf("F1 - Napoveda \t F2 - Start/Restart \t F4 - Koniec\n");
+            }else if(a == 83){ //48
+                return 0;
+            }else if(a == 81){ //50
+                goto game;
+            }
 
 //menu po 1
 
-        b = getch();
-        game:
-        if(b == 81){
-            system("clear"); //clear terminal
-            char randomLetter = (rand()%(122 - 97 + 1)) + 97; //ascii kod pismena
-            char hadaneZnaky[30];
-            int pocetPokusov = 0;
-            printf("%c\n", randomLetter);
+            b = getch();
+            game:
+            if(b == 81){
+                system("clear"); //clear terminal
+                char randomLetter = (rand()%(122 - 97 + 1)) + 97; //ascii kod pismena
+                char hadaneZnaky[30];
+                int pocetPokusov = 0;
+                printf("%c\n", randomLetter);
 
 //  hadame znak a zistujeme cas
-            gettimeofday(&start, NULL);
-            do {
-                hadanyZnak = getch();
-                if(hadanyZnak == 83){
-                    return 0;
-                }
-                hadaneZnaky[pocetPokusov] = hadanyZnak;
-                pocetPokusov++;
-            }while (hadanyZnak != randomLetter);
-            gettimeofday(&stop, NULL);
-            double time;
-            time = (stop.tv_sec - start.tv_sec) * 1000;
-            time += (stop.tv_usec - start.tv_usec) / 1000.0;
-            time /= 1000;
+                gettimeofday(&start, NULL);
+                do {
+                    hadanyZnak = getch();
+                    if(hadanyZnak == 83){
+                        return 0;
+                    }
+                    hadaneZnaky[pocetPokusov] = hadanyZnak;
+                    pocetPokusov++;
+                }while (hadanyZnak != randomLetter);
+                gettimeofday(&stop, NULL);
+                double time;
+                time = (stop.tv_sec - start.tv_sec) * 1000;
+                time += (stop.tv_usec - start.tv_usec) / 1000.0;
+                time /= 1000;
 //  vyherne hlasky
-            if(pocetPokusov == 1){
-                if(time <= 1){
-                    printf("\33[0;32m"); //green
-                    printf("cas bol: %.2f\n", time);
-                    printf("\33[0m");
-                    printf("stlacili ste: %c", hadanyZnak);
-                }else if(time > 1 && time <= 1.5){
-                    printf("\33[0;36m"); //cyan
-                    printf("cas bol: %.2f\n", time);
-                    printf("\33[0m");
-                    printf("stlacili ste: %c", hadanyZnak);
-                }else if(time > 1.5){
-                    printf("\33[0;31m"); //red
-                    printf("cas bol: %.2f\n", time);
-                    printf("\33[0m");
-                    printf("stlacili ste: %c", hadanyZnak);
+                if(pocetPokusov == 1){
+                    if(time <= 1){
+                        printf("\33[0;32m"); //green
+                        printf("cas bol: %.2f\n", time);
+                        printf("\33[0m");
+                        printf("stlacili ste: %c\n", hadanyZnak);
+                        printf("F1 - Napoveda \t F2 - Start/Restart \t F4 - Koniec\n");
+                    }else if(time > 1 && time <= 1.5){
+                        printf("\33[0;36m"); //cyan
+                        printf("cas bol: %.2f\n", time);
+                        printf("\33[0m");
+                        printf("stlacili ste: %c", hadanyZnak);
+                        printf("F1 - Napoveda \t F2 - Start/Restart \t F4 - Koniec\n");
+                    }else if(time > 1.5){
+                        printf("\33[0;31m"); //red
+                        printf("cas bol: %.2f\n", time);
+                        printf("\33[0m");
+                        printf("stlacili ste: %c", hadanyZnak);
+                        printf("F1 - Napoveda \t F2 - Start/Restart \t F4 - Koniec\n");
+                    }
+                }else if(pocetPokusov >= 1){
+                    if(time <= 1){
+                        printf("\33[0;32m");
+                        printf("cas bol: %.2f\n", time);
+                        printf("\33[0m");
+                        printf("Postupne ste stlacali: ");
+                        int i = 0;
+                        while(i < pocetPokusov){
+                            printf("%c ", hadaneZnaky[i]);
+                            i++;
+                        }
+                        printf("\nF1 - Napoveda \t F2 - Start/Restart \t F4 - Koniec\n");
+                    }else if(time > 1 && time <= 1.5){
+                        printf("\33[0;36m");
+                        printf("cas bol: %.2f\n", time);
+                        printf("\33[0m");
+                        printf("Postupne ste stlacali: ");
+                        int i = 0;
+                        while(i < pocetPokusov){
+                            printf("%c ", hadaneZnaky[i]);
+                            i++;
+                        }
+                        printf("\nF1 - Napoveda \t F2 - Start/Restart \t F4 - Koniec\n");
+                    }else if(time > 1.5){
+                        printf("\33[0;31m");
+                        printf("cas bol: %.2f\n", time);
+                        printf("\33[0m");
+                        printf("Postupne ste stlacali: ");
+                        int i = 0;
+                        while(i < pocetPokusov){
+                            printf("%c ", hadaneZnaky[i]);
+                            i++;
+                        }
+                        printf("\nF1 - Napoveda \t F2 - Start/Restart \t F4 - Koniec\n");
+                    }
                 }
-            }else if(pocetPokusov >= 1){
-                if(time <= 1){
-                    printf("\33[0;32m");
-                    printf("cas bol: %.2f\n", time);
-                    printf("\33[0m");
-                    printf("Postupne ste stlacali: ");
-                    int i = 0;
-                    while(i < pocetPokusov){
-                        printf("%c ", hadaneZnaky[i]);
-                        i++;
-                    }
-                }else if(time > 1 && time <= 1.5){
-                    printf("\33[0;36m");
-                    printf("cas bol: %.2f\n", time);
-                    printf("\33[0m");
-                    printf("Postupne ste stlacali: ");
-                    int i = 0;
-                    while(i < pocetPokusov){
-                        printf("%c ", hadaneZnaky[i]);
-                        i++;
-                    }
-                }else if(time > 1.5){
-                    printf("\33[0;31m");
-                    printf("cas bol: %.2f\n", time);
-                    printf("\33[0m");
-                    printf("Postupne ste stlacali: ");
-                    int i = 0;
-                    while(i < pocetPokusov){
-                        printf("%c ", hadaneZnaky[i]);
-                        i++;
-                    }
-                }
+            }else if(b == 83){
+                return 0;
             }
-        }else if(b == 83){
-            return 0;
+            printf("\n");
         }
-        printf("\n");
     }
 }
 
