1deb71358524dafbca1e73c9dbfb9437830bdcd9
CodyM4V3R1CK <bobkomatej1@gmail.com>
2020-12-11 17:41:26 +0100

    testing the new array

--- 3rd-grade/C/dekodovacka/decode.c
+++ 3rd-grade/C/dekodovacka/decode.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 int main(void) {
+    //read from text file and save into hexNumbers
     char hexNumber[1000];
     FILE *hex;
     if ((hex = fopen("intelHEX.txt", "r")) == NULL) {
@@ -10,7 +12,19 @@ int main(void) {
 
     fscanf(hex, "%[^\t]", hexNumber);
     printf("Data from the file:\n%s", hexNumber);
+    printf("\n------------------------------\n");
     fclose(hex);
+
+// int tmp = 0;
+// while (tmp < 1000){
+//     printf("%c", hexNumber[tmp]);
+//     tmp++;
+// }
+
+    // make an array that contains all 3 numbers
+
+    int hexCode[100][3];
+
 
     return 0;
 }
