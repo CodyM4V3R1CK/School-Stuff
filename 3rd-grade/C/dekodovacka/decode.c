381b4b427c0a7ffd850f2856a892865f4cd4cc26
CodyM4V3R1CK <bobkomatej1@gmail.com>
2020-12-11 16:48:14 +0100

    kill me

--- 3rd-grade/C/dekodovacka/decode.c
+++ 3rd-grade/C/dekodovacka/decode.c
@@ -5,12 +5,10 @@ int main(void) {
     FILE *hex;
     if ((hex = fopen("intelHEX.txt", "r")) == NULL) {
         printf("Error! opening file");
-        // Program exits if file pointer returns NULL.
         exit(1);
     }
 
-    // reads text until newline is encountered
-    fscanf(hex, "%[^\n]", hexNumber);
+    fscanf(hex, "%s", hexNumber);
     printf("Data from the file:\n%s", hexNumber);
     fclose(hex);
 
