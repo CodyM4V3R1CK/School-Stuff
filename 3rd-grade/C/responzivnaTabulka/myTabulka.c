62a543f39f5afd63e1a545463df8bf3dbfbf4f87
CodyM4V3R1CK <bobkomatej1@gmail.com>
2021-01-17 15:59:45 +0100

    bit of cheeky code "inspiration"

--- 3rd-grade/C/responzivnaTabulka/myTabulka.c
+++ 3rd-grade/C/responzivnaTabulka/myTabulka.c
@@ -7,6 +7,8 @@ typedef struct zaznam{
     int tel;
 } Zaznam;
 
+void decimal2roman(int num);
+
 int main (void){
 
     Zaznam clovek[1];
@@ -23,10 +25,21 @@ int main (void){
     printf("zadaj telefonne cislo v tvare (xxxxxxxxxx bez medzier): ");
     scanf(" %d", &clovek[1].tel);
 
-    printf("%s\n", clovek[1].meno);
-    printf("%d\n", clovek[1].vyska);
-    printf("%d\n", clovek[1].rokNarodenia);
-    printf("%d\n", clovek[1].tel);
+    // decimal2roman(clovek[1].rokNarodenia);
 
     return 0;
+}
+
+void decimal2roman(int num){
+    int decimal[] = {1000,900,500,400,100,90,50,40,10,9,5,4,1}; //base values
+    char *symbol[] = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};  //roman symbols
+    int i = 0;
+ 
+    while(num){ //repeat process until num is not 0
+        while(num/decimal[i]){  //first base value that divides num is largest base value
+            printf("%s",symbol[i]);    //print roman symbol equivalent to largest base value
+            num -= decimal[i];  //subtract largest base value from num
+        }
+        i++;    //move to next base value to divide num
+    }
 }
